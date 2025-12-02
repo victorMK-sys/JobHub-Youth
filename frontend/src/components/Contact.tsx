d11ff64f8af4bd415bdb8c4bb02f2fee0fc2fe0a
VictorMK-sys <victorkilonzi0@gmail.com>
2025-12-02 11:43:52 +0300

    Update on Contact

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -17,7 +17,6 @@ function Contact() {
               type="email"
               id="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
               required
             />
           </div>
@@ -32,7 +31,6 @@ function Contact() {
               type="text"
               id="subject"
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Let us know how we can help you"
               required
             />
           </div>
@@ -46,7 +44,6 @@ function Contact() {
             <textarea
               id="message"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Leave a comment..."
             ></textarea>
           </div>
           <button
